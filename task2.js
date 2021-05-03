4aa7e3a6bba4d52bca84d31d93cabc0338a14c21
Aleksey <depi_games@mail.ru>
2021-05-03 09:42:38 +0300

    Второе задание

---
+++ task2.js
@@ -0,0 +1,26 @@
+// Задание 1
+let radius = prompt("Введите радиус окружности:");
+let Pi = 3.14;
+let circleLength = 2 * Pi * radius;
+alert(`Длина окружности = ${circleLength}`);
+
+// Задание 2
+let secondsInMinutes = 60;
+let MinutesInHour = 60;
+let secondsInHour = secondsInMinutes * MinutesInHour;
+alert(`${secondsInHour} секунд в одном часе`);
+
+// Задание 3
+let num = 47;
+num += 7;
+num -= 18;
+num *= 10;
+num /= 15;
+alert(`Задание 3, num = ${num}`);
+
+// Задание 4
+var num2 = 10;
+++num2;
+num += 1;
+--num2;
+alert(`Задание 4, num = ${num2}`);
